a83b2dc99a09406273b8044843019ea1eeaed78c
frekerzzzz <kannamugesh135@gmail.com>
2024-03-08 20:31:22 +0530

    done

--- app.js
+++ app.js
@@ -8,16 +8,7 @@ const PORT = process.env.PORT || 5000;
 const bcrypt = require("bcryptjs")
 
 app.use(cors());
-app.use(bodyParser.json());
-
-
-mongoose.connect('mongodb://localhost:27017/your-database-name', {
-})
-.then(() => console.log('MongoDB connected'))
-.catch(err => console.error(err));
-
-
-
+app.use(bodyParser.json())
 
 const auth = require("./routes/auth")
 app.use("/",auth)
